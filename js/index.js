a6d9f8ac25d3a7056bf1fd15cdc9a95063c5f1ce
Fer Sánchez <sangarfer@gmail.com>
2022-01-27 20:23:00 +0100

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+var background = new Image();
+background.src = "images/road.png";
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(background,0,0);
+  }
 };
+
